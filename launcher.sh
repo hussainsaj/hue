31c9567da30b8c7e7b3c599fdc5fe29c47083414
hussainsaj <hussain-sajid@outlook.com>
2025-01-19 08:10:44 +0000

    update

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Activate the virtual environment
+source /home/hussain/Desktop/hue/venv/bin/activate
+
+# Change to the hue directory
 cd /home/hussain/Desktop/hue
+
+# Run the Python script and log output
 python3 bulb.py >> /home/hussain/Desktop/hue/logs/bulb.log 2>&1 &
-cd /
